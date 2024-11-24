744ada1b304a6daaa4b7acf24eef0e957cff92f3
Pranav Aditya Seelam <paseelam@gmail.com>
2024-11-24 15:08:36 -0800

    Self-attention

--- Building_GPT/bigram.py
+++ Building_GPT/bigram.py
@@ -65,12 +65,14 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
 
         # idx and targets are both (B,T) tensor of integers
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emd = self.position_embedding_table(torch.arange(tok_emb, device=device))
         logits = self.lm_head(tok_emb) #(B, T, Vocab_size)
 
         if targets is None:
